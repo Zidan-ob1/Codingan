dc9d4dcff1fcc1d3d2e03db27ccb8de224c039d7
Zidan-ob1 <zidanewibowo08@gmail.com>
2024-07-18 16:41:33 +0700

    Add files via upload

---
+++ 2.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+
+public class Hewan
+{
+    public int id { get; set; }
+    public string Nama { get; set; }
+    public string Jenis {  get; set; }
+    public string Golongan { get; set; }
+   
+}
+
+public class Program
+{
+    static void Main(String[] args)
+    {
+       Hewan[] objek = new Hewan[4];
+
+
+        Hewan number1 = new Hewan { id = 1, Nama = "Maine Coon", Jenis = "Kucing", Golongan = "Karnivora" };
+        Hewan number2 = new Hewan { id = 2, Nama = "Elang", Jenis = "Burung", Golongan = "Karnivora" };
+        Hewan number3 = new Hewan { id = 3, Nama = "Parkit", Jenis = "Burung", Golongan = "Herbivora" };
+        Hewan number4 = new Hewan { id = 4, Nama = "Paus", Jenis = "Ikan", Golongan = "Karnivora" };
+
+        objek[0] = number1;
+        objek[1] = number2;
+        objek[2] = number3;
+        objek[3] = number4;
+
+        foreach (Hewan binatang in objek)
+        {
+            Console.WriteLine($"id: {binatang.id}, Nama: {binatang.Nama}, Golongan: {binatang.Golongan}, Jenis: {binatang.Jenis}");
+    
+        }  
+    }
+}
+
---
+++ 3.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+
+public class Hewan
+{
+    public int id { get; set; }
+    public string Nama { get; set; }
+    public string Jenis { get; set; }
+    public string Golongan { get; set; }
+
+}
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        List<Hewan> list = new List<Hewan>
+        {
+            new Hewan { id = 1, Nama = "Maine Coon", Jenis = "Kucing", Golongan = "Karnivora" },
+            new Hewan { id = 2, Nama = "Elang", Jenis = "Burung", Golongan = "Karnivora" },
+            new Hewan { id = 3, Nama = "Parkit", Jenis = "Burung", Golongan = "Herbivora" },
+            new Hewan { id = 4, Nama = "Paus", Jenis = "Ikan", Golongan = "Karnivora" },
+         };
+
+        Console.WriteLine("List Hewan: ");
+        foreach(var hewan in list)
+        {
+            Console.WriteLine($" id: {hewan.id}, Nama: {hewan.Nama}, Jenis: {hewan.Jenis}, Golongan: {hewan.Golongan}");
+        }
+
+       
+        Hewan databaru = new Hewan();
+        Console.WriteLine("Masukkan Data baru: ");
+
+        Console.Write("Id: ");
+        databaru.id = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Nama: ");
+        databaru.Nama = Console.ReadLine();
+
+        Console.Write("Jenis: ");
+        databaru.Jenis = Console.ReadLine();
+
+        Console.Write("Golongan: ");
+        databaru.Golongan = Console.ReadLine();
+
+        
+        list.Add(databaru);
+
+       
+        Console.WriteLine("\nUpdated List Hewan: ");
+        foreach (var hewan in list)
+        {
+            Console.WriteLine($" id: {hewan.id}, Nama: {hewan.Nama}, Jenis: {hewan.Jenis}, Golongan: {hewan.Golongan}");
+        }
+    }
+
+}
+
